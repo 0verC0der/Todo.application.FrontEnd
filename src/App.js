e0f737c6749380ef018dc4c9f70f3419dbb3b91d
0verC0der <nikitocc.ka@gmail,com>
2022-02-11 18:06:41 +0300

    todo-app-frontend

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import logo from './logo.svg';
+import './App.css';
+import { Fragment, useEffect, useState } from 'react';
+import TodoItem from './components/todoItem';
+
+function App() {
+  const [todoItems, setTodoItems] = useState(null);
+  const [users, setUsers] = useState(null);
+
+  useEffect(()=> {
+
+    if(!todoItems) {
+      fetch('http://localhost:8080/api/todoItems')
+        .then(response => response.json())
+        .then(data => {
+          //console.log(data);
+          setTodoItems(data);
+        });
+
+        fetch('http://localhost:8080/api/users')
+        .then(response => response.json())
+        .then(data => {
+          //console.log(data);
+          setUsers(data);
+        });
+    }
+  }, [todoItems], [users]);
+
+  function addNewTodoItem(){
+    fetch(`http://localhost:8080/api/todoItems/`, {
+      method: "POST",
+      headers: {
+          "content-type": "application/json",
+      }, 
+      body: JSON.stringify('rr')
+    }).then(response => response.json())
+      .then(data => {
+         setTodoItems([...todoItems, data]);
+      });
+  }
+
+  function handleDeleteTodo(item){
+    const uptadeTodoItems = todoItems.filter(aTodo => aTodo.id !== item.id);
+    setTodoItems([...uptadeTodoItems]);
+  }
+    
+  return (
+    <>
+        <div>
+          <label>
+              Add new task 
+              <input type="button" value="Ok" name="ok_btn" onClick={addNewTodoItem}/>
+
+            </label>
+        </div>
+
+        <div> 
+          {todoItems ? todoItems.map (todoItem => {
+          return <TodoItem key={todoItem.id} data={todoItem} emitDeleteTodoItem={handleDeleteTodo} />}) 
+          : 'loading data...'}
+        </div> 
+
+        <div>
+          {users ? users.map(user => {
+            return (
+              <Fragment key={user.id}>
+                <span>{user.username}</span>
+              </Fragment>
+            )
+          }) : 'loading data-user...'}
+        </div>
+      
+    </>   
+  );
+}
+
+export default App;
